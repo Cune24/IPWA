2bef40d640ed7260bc267895a3d91e175de38107
Cune24 <cunechan24@gmail.com>
2026-07-05 12:36:23 +0200

    Added Documentation + increased size of button on mobile 	modified:   style.css 	modified:   textDirection.js

--- textDirection.js
+++ textDirection.js
@@ -2,12 +2,13 @@ const toggleBtn = document.getElementById('toggleDirectionBtn');
 
 if (toggleBtn) {
   toggleBtn.addEventListener('click', () => {
+
     const currentDir = document.documentElement.getAttribute('dir');
-    const newDir = currentDir === 'rtl' ? 'ltr' : 'rtl';
-    
+    const newDir = currentDir === 'rtl' ? 'ltr' : 'rtl'; 
     document.documentElement.setAttribute('dir', newDir);
     document.body.setAttribute('dir', newDir);
     
+    //Wenn die neue Richtung "rtl" ist wird der Button in Rückgängig benannt, ansonsten wieder "Richtung wechseln"
     toggleBtn.textContent = newDir === 'rtl' ? 'Rückgängig' : 'Richtung wechseln';
   });
 }
